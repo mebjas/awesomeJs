db52eef33106e0e9e4cff064185ba1a280b1b862
minhaz <minhazav@gmail.com>
2014-04-26 10:09:44 +0530

    coding style modified

--- smoothscroll/awesomejs.smoothscroll.js
+++ smoothscroll/awesomejs.smoothscroll.js
@@ -1,14 +1,20 @@
-
+/**
+ * awesomeJs.smoothScroll.js
+ * converts any internal anchor tag, to source of smooth js transition.
+ * just include this script in your page & it will automatically add
+ * the mentioned feature.
+ * Dependency: jQuery library
+ */
 $(document).ready(function() {
 	$("a").click(function(event) {
-		if($(this).attr("href").substr(0, 1) === "#") {
+		if ($(this).attr("href").substr(0, 1) === "#") {
 			
 			//prevent the default action
 			event.preventDefault();
 
 			//take document scroll to this position
-			$('html,body').animate( {
-	        scrollTop: $("a[name='"+$(this).attr("href").replace("#", "")+"']").offset().top},
+			$('html,body').animate({
+	        scrollTop: $("a[name='" +$(this).attr("href").replace("#", "") +"']").offset().top},
 	        1000);
 		}
 	});
